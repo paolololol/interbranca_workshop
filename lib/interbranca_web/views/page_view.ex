e44d17aed566a14f080c2fcb7ab11a3e30d9a414
Paolo Campanelli <paolo.campanelli@appfire.com>
2022-09-10 10:18:29 +0200

    Colori

--- lib/interbranca_web/views/page_view.ex
+++ lib/interbranca_web/views/page_view.ex
@@ -29,4 +29,13 @@ defmodule InterbrancaWeb.PageView do
       Enum.count(iscrizioni) < 2
     end
   end
+
+  def get_color(text) do
+    cond do
+     text == "Gioca il gioco" -> "#ff03ff"
+     text == "ComExpress" -> "#45818e"
+     text == "StarBene" -> "#ff9f11"
+     text == "Scientificamente scout" -> "#980000"
+    end
+  end
 end
